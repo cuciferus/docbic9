ca899d8fdffb0becff07b190cacad6d2c08b25f5
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-06 01:24:07 +0200

    greseala

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,6 @@ class SessionsController < ApplicationController
   def create
     session[:access_token] = '28430602-a5F6mvn4FafyxbL9tu7rezTq2GuRvQaQ5QXqjZq2x'
     session[:acess_secret] = 'HwJ4sV7hpOvIGcvvVssOLTywxjwATKHpCJYooPFF7U'
-    redirect_to root, :notice => "Logat prin twitter'
+    redirect_to root_path
   end
 end
