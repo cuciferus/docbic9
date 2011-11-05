16f8502c2596dd4346d75063a46d97d09e21de52
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-06 00:46:43 +0200

    typo fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.use OmniAuth.builder do
+Rails.application.config.middleware.use OmniAuth.Builder do
   use OmniAuth::Strategies::Twitter , 'orpdVK584HAHyekm3I0eFA','MfEPqv3OrFoyKuh7dFUgYrM5YUT1wnCmyPpjvARzOtk'
 end
