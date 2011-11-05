2e14f342747b9a0c0b79cbbf9fd771e66a71bd6b
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-06 01:19:12 +0200

    inca modificari de twitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  private
+  def client
+    Twitter.configure do |config|
+      config.consumer_key = 'orpdVK584HAHyekm3I0eFA'
+      config.consumer_secret = 'MfEPqv3OrFoyKuh7dFUgYrM5YUT1wnCmyPpjvARzOtk'
+      config.auth_token = session['access_token']
+      config.auth_token_secret = session['access_secret']
+    end
+    @client || = Twitter::Client.new
+  end
 end
