0dc7a113e6584e1d4aab56a7a02260a692b80060
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-12 00:27:04 +0200

    corectie controller si lucru la css

--- app/controllers/pacients_controller.rb
+++ app/controllers/pacients_controller.rb
@@ -1,6 +1,7 @@
 class PacientsController < InheritedResources::Base
   respond_to :html, :js
   respond_to :pdf, :only => :consent
+
   def consent
     respond_to do |format|
       format.pdf do
@@ -13,34 +14,25 @@ class PacientsController < InheritedResources::Base
   def index
     @pacients = Pacient.paginate(:page => params[:page])
   end
-  def screening
-     @pacient = Pacient.find(params[:id])
- end
-=begin
- def create #asta nu e bine! trebuie refacut redirectu
-     create! do |success, failure|
-       success.html {new_pacient_vizitum_path(@pacient, :vizita => "screening")}
-       failure.html {new_pacient_vizitum_path(@pacient, :vizita => "screening")}
-     end
- end
- reluare de faza
-=end
-   def create
+
+  def create
      @pacient = Pacient.new(params[:pacient])
      respond_to do |format|
        if @pacient.save
-         format.html {redirect_to new_pacient_vizitum_path(@pacient)}
+         format.html {redirect_to new_pacient_vizitum_path(@pacient, :vizita => "screening" )}
        else
          format.html { render :action => "new"}
        end
      end
-   end
- def randomizare
+  end
+
+  def randomizare
    #incomplet trebuie verificat
    @pacient = Pacient.find(params[:id])
    @anamneza = @pacient.anamnezas.build
    @clinic = @pacient.clinics.build
- end
+  end
+
   def includere #asta nu imi mai e util trebuie merged into REST
     @pacient = Pacient.find(params[:id])
   end
