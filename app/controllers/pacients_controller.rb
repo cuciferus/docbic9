e8591eb2d4026402fae5a35f4b4845f01f5db28c
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-29 07:30:05 +0200

    cleanup

--- app/controllers/pacients_controller.rb
+++ app/controllers/pacients_controller.rb
@@ -27,15 +27,6 @@ class PacientsController < InheritedResources::Base
      end
   end
 
-  def randomizare
-   #incomplet trebuie verificat
-   @pacient = Pacient.find(params[:id])
-   @anamneza = @pacient.anamnezas.build
-   @clinic = @pacient.clinics.build
-  end
 
-  def includere #asta nu imi mai e util trebuie merged into REST
-    @pacient = Pacient.find(params[:id])
-  end
 
 end
