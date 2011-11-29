8e5809455539fcd003547e7887a6f4e9970b345b
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-29 20:18:00 +0200

    validare mai buna pt numere de telefon

--- app/models/pacient.rb
+++ app/models/pacient.rb
@@ -6,8 +6,8 @@ class Pacient < ActiveRecord::Base
   accepts_nested_attributes_for :vizita
   validates :semnat_icf, :acceptance => true #, :message => "E musai ca pacientul să semneze inform consentul"
   validates :cnp, :length => { :is =>13, :wrong_length => "CNP are 13 cifre" }, :numericality => { :only_integer =>true}
-  validates :telefon1, :length => { :is =>10, :wrong_length => "Un număr de telefon în România are 10 cifre"}, :numericality => { :only_integer =>true}, :allow_blank => true
-  validates :telefon2, :length => { :is =>10, :wrong_length => "Un număr de telefon în România are 10 cifre"}, :numericality => { :only_integer =>true}, :allow_blank => true
+  validates_format_of :telefon1, :with => /\d{10}/, :message => "Un număr de telefon în România are 10 cifre"
+  validates_format_of :telefon2, :with => /\d{10}/, :message => "Un număr de telefon în România are 10 cifre"
   def self.search(search, page) #tot nu merge
     if search
       paginate :per_page => 5, :page => page,
