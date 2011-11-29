da028f306fd86d2d77193aeae5cf0b238654f30b
Cucu Ionut <ionutcristiancucu@gmail.com>
2011-11-29 08:21:08 +0200

    pacient show schita, arata groaznic

--- app/assets/javascripts/pacients.js.coffee
+++ app/assets/javascripts/pacients.js.coffee
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//=require jquery.validate.min
 $(document).ready ->
    $("#pacient_data_dg_ckd").datepicker({dateFormat: 'yyyy-mm-dd'})
    $("#pacient_data_dg_diabet").datepicker()
+   $("#simple_form").validate()
--- app/models/pacient.rb
+++ app/models/pacient.rb
@@ -8,9 +8,11 @@ class Pacient < ActiveRecord::Base
   validates :cnp, :length => { :is =>13, :wrong_length => "CNP are 13 cifre" }, :numericality => { :only_integer =>true}
   validates :telefon1, :length => { :is =>10, :wrong_length => "Un număr de telefon în România are 10 cifre"}, :numericality => { :only_integer =>true}, :allow_blank => true
   validates :telefon2, :length => { :is =>10, :wrong_length => "Un număr de telefon în România are 10 cifre"}, :numericality => { :only_integer =>true}, :allow_blank => true
-  def self.search(search, page)
+  def self.search(search, page) #tot nu merge
     if search
-      find(:all, :conditions => ['nume LIKE ?', "%#{search}"])
+      paginate :per_page => 5, :page => page,
+        :conditions => ['nume like ?', "%#{search}%"], :order => 'nume'
+      #find(:all, :conditions => ['nume LIKE ?', "%#{search}"])
     else
       find(:all)
     end
